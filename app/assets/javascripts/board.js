141506c54b72112fd75e1b28e05b65f33c786465
Jessica J. Kim <jessjkim@gmail.com>
2014-01-20 19:49:27 -0800

    Add some board class logic and CSS to switch gems.

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -5,6 +5,7 @@
 		this.dimension = 8;
     this.renderBoard();
     this.renderGems();
+    this.clickedGems = [];
 	};
   
 	Board.prototype.renderBoard = function() {
@@ -15,6 +16,8 @@
 			}
 		}
 	};
+  
+
 
 	Board.prototype.renderGems = function() {
     this.gemList = ["red", "green", "yellow", "silver", "orange", "pink"];    
@@ -29,6 +32,69 @@
 		}
 	};
   
-	
+  Board.prototype.handleClick = function(event) {
+    var gem = $(event.currentTarget)
+    
+    var gem_color = $(event.currentTarget).attr("class").split(" ")[1]
+    if (this.clickedGems.length == 2) {
+      this.clearClickedGems();
+    }
+    
+    if (this.clickedGems.length < 1) {
+      this.clickedGems.push(gem);
+    }
+    else if (this.clickedGems.length == 1) {
+      // push the gem in the array
+      this.clickedGems.push(gem);
+      
+      // check the gems to see if we should switch
+      this.checkGems();
+    }
+  };
+  
+  Board.prototype.checkGems = function() {
+    var gem1 = this.clickedGems[0];
+    var gem2 = this.clickedGems[1];
+    
+    // first check if the gems are next to each other
+    if (this.checkGemsLocation(gem1, gem2)) {
+      this.checkGemSwitch(gem1, gem2);
+    }
+  };
+  
+  Board.prototype.clearClickedGems = function() {
+    this.clickedGems = [];
+  };
+  
+  Board.prototype.checkGemsLocation = function(gem1, gem2) {
+    var gem1_location = gem1.attr("id").split("row")[1].split("col");
+    var gem2_location = gem2.attr("id").split("row")[1].split("col");
+    if (gem1_location[0] != gem2_location[0] && gem1_location[1] != gem2_location[1]) {
+      return false;
+      this.clearClickedGems();
+    }
+    else {
+      if (gem1_location[0] == gem2_location[0]) {
+        return (Math.abs(gem1_location[1] - gem2_location[1]) == 1)
+      }
+      else if (gem1_location[1] == gem2_location[1]) {
+        return (Math.abs(gem1_location[0] - gem2_location[0]) == 1)
+      }
+    }
+  };
+  
+	Board.prototype.checkGemSwitch = function(gem1, gem2) {
+    var gem1_color = $(gem1).attr("class").split(" ")[1]
+    var gem2_color = $(gem2).attr("class").split(" ")[1]
+    
+    $(gem1).removeClass(gem1_color).addClass(gem2_color);
+    $(gem2).removeClass(gem2_color).addClass(gem1_color);
+    this.checkGemline(gem1, gem2);
+	};
+  
+  // come back here
+  Board.prototype.checkGemline = function(gem1, gem2) {
+    debugger
+  };
 
 })(this);
